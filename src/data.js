ebddd4c9d53e6daa20730ad6635c860c458f5ada
Irfan Maulana <mazipanneh@gmail.com>
2019-06-25 16:42:27 +0700

    📝 init data store

---
+++ src/data.js
@@ -0,0 +1,32 @@
+export default {
+  profile: {
+    name: 'Irfan Maulana',
+    title: 'Principal Engineer for Frontend at Tokopedia',
+    photo: 'https://avatars2.githubusercontent.com/u/7221389?s=300&v=4', // 300x300px
+    city: 'Jakarta, Indonesia',
+    phone: '(+62) 857-8123-8798',
+    mail: 'mazipanneh@gmail.com'
+  },
+  social: {
+    facebook: {},
+    twitter: {},
+    linkedin: {},
+    github: {}
+  },
+  summary: 'An experience software engineer especially in frontend side. Having deep knowledge in HTML, CSS (with it's pre-processor) and JavaScript with various framework. Write good quality, well tested, and fast delivered code. Experience in architecting frontend codebase.<br> Well known figure in programming community, open-source creator, and writer for many technical article.',
+  experiences: [
+  
+  ],
+  educations: [
+  
+  ],
+  projetcs: [
+  
+  ],
+  skills: [
+  
+  ],
+  languages: [
+  
+  ]
+}
