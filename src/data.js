48767410f014dffd01792d72a792d844532df255
Irfan Maulana <mazipanneh@gmail.com>
2019-06-25 16:47:35 +0700

    Add experiences

--- src/data.js
+++ src/data.js
@@ -13,9 +13,50 @@ export default {
     linkedin: {},
     github: {}
   },
-  summary: 'An experience software engineer especially in frontend side. Having deep knowledge in HTML, CSS (with it's pre-processor) and JavaScript with various framework. Write good quality, well tested, and fast delivered code. Experience in architecting frontend codebase.<br> Well known figure in programming community, open-source creator, and writer for many technical article.',
+  summary: 'An experience software engineer especially in frontend side. Having deep knowledge in HTML, CSS (with it\'s pre-processor) and JavaScript with various framework. Write good quality, well tested, and fast delivered code. Experience in architecting frontend codebase.<br> Well known figure in programming community, open-source creator, and writer for many technical article.',
   experiences: [
-  
+    {
+      company: 'Tokopedia',
+      location: 'Jakarta',
+      title: 'Principal Engineer for Frontend',
+      duration: 'JUN 2019 - PRESENT',
+      jobdesc: [],
+    },
+    {
+      company: 'Bizzy Indonesia',
+      location: 'Jakarta',
+      title: 'Software Architect for Frontend',
+      duration: 'FEB 2019 - JUN 2019',
+      jobdesc: [],
+    },
+    {
+      company: 'Bizzy Indonesia',
+      location: 'Jakarta',
+      title: 'Software Development Engineer',
+      duration: 'JUL 2018 - FEB 2019',
+      jobdesc: [],
+    },
+    {
+      company: 'Blibli.com',
+      location: 'Jakarta',
+      title: 'Sr. Software Development Engineer',
+      duration: 'JAN 2017 - JUN 2018',
+      jobdesc: [],
+    },
+    {
+      company: 'Blibli.com',
+      location: 'Jakarta',
+      title: 'Software Development Engineer',
+      duration: 'OCT 2015 - JAN 2017',
+      jobdesc: [],
+    },
+    {
+      company: 'SML Technologies',
+      location: 'Jakarta',
+      title: 'Java Developer',
+      duration: 'AUG 2013 - OCT 2015',
+      jobdesc: [],
+    }
   ],
   educations: [
   
